8d89d8dfa94036ea5c3ae2d654ba7ee1727cd303
unknown <minhtam30102002@gmail.com>
2022-07-24 19:39:08 +0700

    Second Commit

--- app/models/user_test.go
+++ app/models/user_test.go
@@ -1,30 +1,34 @@
 package models
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestUser(t *testing.T) {
 	user := User{
-		ID: 1,
-		EntityCode: 1,
-		Username: "minhtam",
-		Fullname: "Le Minh Tam",
-		Password: "999",
-		Email: "tam@gmail.com",
-		Address: "None",
-		BOD: "30/10/2002",
-		Phone: "9999",
+		ID:            1,
+		EntityCode:    1,
+		Username:      "minhtam",
+		Fullname:      "Le Minh Tam",
+		Password:      "999",
+		Email:         "tam@gmail.com",
+		Address:       "None",
+		BOD:           "30/10/2002",
+		Phone:         "9999",
 		Qualification: "None",
-		Slogan: "None",
-		Role: "Dev",
-		Hobby: "None",
-		DateCreated: 1658576973,
-		DateUpdated: 1658576973,
+		Slogan:        "None",
+		Role:          "Dev",
+		Hobby:         "None",
+		DateCreated:   1658665531,
+		DateUpdated:   1658665531,
 	}
 
 	result := GetUserById(1, "Le Minh Tam", "minhtam")
 	if result != user {
 		t.Error("Error :(((")
-	}else {
+	} else {
 		t.Log("PASSED")
 	}
+	fmt.Println("Minh Tam")
 }
